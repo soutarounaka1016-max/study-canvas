2d35f214260aefb9e6ec58788bc7e6638fbb8428
soutarounaka1016-max <soutarou.naka.1016@gmail.com>
2026-07-20 15:21:46 +0900

    Add taskization entrypoint

---
+++ taskize-entry.js
@@ -0,0 +1,10 @@
+const existingStyle = document.querySelector('link[data-taskize-style="true"]');
+if (!existingStyle) {
+  const style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = "taskize.css?v=20260720-10";
+  style.dataset.taskizeStyle = "true";
+  document.head.append(style);
+}
+
+await import("./taskize-ui.js?v=20260720-10");
