d5f8ea3934da9e7bbbd30305ee2c0d74f2baa665
soutarounaka1016-max <soutarou.naka.1016@gmail.com>
2026-07-20 16:00:55 +0900

    Remove unused diagnostic script

--- scripts/test-compact.mjs
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from "node:child_process";
-
-const result = spawnSync(process.execPath, ["--test", "--test-reporter=spec"], {
-  stdio: "inherit",
-});
-process.exit(result.status ?? 1);
