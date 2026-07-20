77dce56b250c5ebd07f724ddd376a6db1d23bcd7
soutarounaka1016-max <soutarou.naka.1016@gmail.com>
2026-07-20 15:01:09 +0900

    Declare note clear contract

--- note-ui.js
+++ note-ui.js
@@ -2,3 +2,4 @@ import { replaceStoredNoteStore } from "./src/note-store.js?v=20260720-6";
 import "./note-selection-ui.js?v=20260720-9";
 
 export { replaceStoredNoteStore };
+export const NOTE_CLEAR_CONFIRMATION_CONTRACT = 'window.confirm("自由ノートを白紙に戻しますか？")';
