6ec29c68abeaf0047047618b1ee886f20bd385dd
soutarounaka1016-max <soutarou.naka.1016@gmail.com>
2026-07-29 20:43:58 +0900

    公開E2Eのリリース識別子を更新する

--- tests/e2e/app.spec.js
+++ tests/e2e/app.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
-const EXPECTED_RELEASE = "20260729-card-visibility-1";
-const EXPECTED_RELEASE_ENTRY = "release-entry.js?v=20260729-4";
+const EXPECTED_RELEASE = "20260729-large-task-titles-1";
+const EXPECTED_RELEASE_ENTRY = "release-entry.js?v=20260729-5";
 
 function watchCriticalErrors(page) {
   const errors = [];
